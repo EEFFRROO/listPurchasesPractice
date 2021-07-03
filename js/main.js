827665882227075496a488556494a693fda852a2
Kirill Efremov <kerish2014@gmail.com>
2021-07-03 20:20:21 +0400

    can add new purchases

--- js/main.js
+++ js/main.js
@@ -3,23 +3,59 @@ $(document).ready(() => {
     // $("body").append("<div class='test'/>");
     main();
 
+    const namePurchase = $(".nameNewPurchase")[0];
+    const pricePurchase = $(".priceNewPurchase")[0];
+    const amountPurchase = $(".amountNewPurchase")[0];
+    const totalNewPurchase = $(".totalNewPurchase")[0];
+
+    $(".priceNewPurchase").on("input", changeTotal);
+    $(".amountNewPurchase").on("input", changeTotal);
+    // amountPurchase.onchange = changeTotal;
+
+
+    $(".addNewPurchaseBtn").click(clickAddNewPurchase);
+
     function main() {
         createNewPurchase("Название", "Цена", "Количество", "Стоимость");
 
+
         createNewPurchase("Помидоры", 57, 2);
         createNewPurchase("Чипсы", 105, 3);
+        createNewPurchase("Большая пачка чипсов", 55, 4);
         createFinal();
     }
 
     function clickAddNewPurchase() {
+        if (namePurchase.value && pricePurchase.value && amountPurchase.value) {
+            if (isNaN(parseFloat(pricePurchase.value)))
+                alert("Неверное введена цена");
+            else if (isNaN(parseInt(amountPurchase.value)))
+                alert("Неверное введено количество");
+            else {
+                createNewPurchase(namePurchase.value, pricePurchase.value, amountPurchase.value);
+                createFinal();
+                namePurchase.value = "";
+                pricePurchase.value = "";
+                amountPurchase.value = "1";
+            }
+        } else {
+            alert("Заполните все поля");
+        }
+    }
 
+    function changeTotal() {
+        price = pricePurchase.value;
+        amount = amountPurchase.value;
+        if (!isNaN(parseFloat(price)) && !isNaN(parseInt(amount))) {
+            totalNewPurchase.value = parseFloat(price) * parseInt(amount);
+        }
     }
 
     function createNewPurchase(name, price, amount, header = null) {
         purchase = createDiv(null, "purchase");
         if (!header) {
-            purchaseTotal = createDiv(parseFloat(price) * parseInt(amount), "purchaseItem total");
             headerClass = "";
+            purchaseTotal = createDiv(parseFloat(price) * parseInt(amount), "purchaseItem total");
         } else {
             headerClass = " purchaseHeader";
             purchaseTotal = createDiv(header, "purchaseItem" + headerClass);
@@ -37,7 +73,7 @@ $(document).ready(() => {
 
     function createFinal() {
         $(".finalPurchase").remove();
-        purchase = createDiv(null, "finalPurchase");
+        purchase = createDiv(null, "finalPurchase purchase");
         finalName = createDiv("Итого", "purchaseItem finalName");
         sum = 0;
         $(".total").each(function() {
